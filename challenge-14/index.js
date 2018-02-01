30cda7d29399cc6dce7b7d4db049f4d5f75e32fd
Jordan Van Ness <jordanwvn@gmail.com>
2018-02-01 15:26:31 -0800

    all tests are passing.

---
+++ challenge-14/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Stack = require ('./lib/stack.js');
+const SLL = require('./lib/sll.js');
+
+const dedupe = module.exports = function (list) { // O(n), iterates once
+  if (!list.value) return null;
+
+  let stack = new Stack;
+  let result = new SLL;
+
+  for (let current = list; current; current = current.next) {
+    if (!stack.top || current.value !== stack.top.value ) {
+      stack.push(current.value);
+      result.insertEnd(current.value);
+    }
+  }
+  return result;
+}
